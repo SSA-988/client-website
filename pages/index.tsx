b6c7aec8203a551912812175b1365706a43fd10b
sujanAnand <sujananand@sujans-MacBook-Pro.local>
2023-04-17 08:44:54 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ type Props = {
   socials:Social[]
 }
 
-const Home = ({experiences,skills,projects,socials}:Props) => {
+const Home = ({experiences,skills,projects,socials,pageInfo}:Props) => {
 
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]'>
@@ -38,12 +38,12 @@ const Home = ({experiences,skills,projects,socials}:Props) => {
       {/* Hero */}
 
       <section id="hero" className='snap-center'>
-        <Hero />
+        <Hero pageInfo={pageInfo}/>
       </section>
 
       {/* About */}
       <section id="about" className='snap-center'>
-        <About />
+        <About pageInfo={pageInfo}/>
       </section>
 
       {/* Experience */}
