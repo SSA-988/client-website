97254f089a2bb629c028d3e2f329b315a47f2b7f
sujanAnand <sujananand@sujans-MacBook-Pro.local>
2023-04-17 08:21:23 +0530

    few changes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -22,7 +22,7 @@ function Contact({ }: Props) {
         <div className='h-screen flex relative flex-col text-center md:text-left md:text-row max-w-7xl justify-evenly mx-auto items-center'>
             <h3 className='absolute top-24 tracking-[20px] uppercase text-gray-500 text-2xl'>Contact</h3>
             <div className='flex flex-col space-y-7 mt-24'>
-                <h4 className='text-2xl font-semibold text-center'>We've have Got just what you need!</h4>
+                <h4 className='text-2xl font-semibold text-center'>We have have Got just what you need!</h4>
                 <div className='space-y-4'>
                     <div className='flex items-center space-x-5 justify-center'>
                         <PhoneIcon className='h-8 w-8 animate-pulse' />
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ type Props = {
 }
 
 const Home = ({pageInfo,experiences,skills,socials,projects}:Props) => {
-  console.log(pageInfo)
+
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]'>
       <Head>
